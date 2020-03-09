f19870864db923bb7910481396d0f5411eb9cad9
Kai Chang <kaijchang@gmail.com>
2020-03-09 10:58:51 -0700

    add lowell mock primary

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,29 +6,30 @@ import Helmet from 'react-helmet';
 
 import { graphql } from 'gatsby';
 
-const Project = ({ fluid, repo, link, description, techonologies }) => (
+const Project = ({ fluid, name, repo, link, description }) => (
     <Card>
-        <a href={ link } target='_blank' rel='noopener noreferrer'>
-            <CardBody>
-                <GatsbyImage fluid={ fluid } style={ { maxWidth: 550 } }/>
-                <CardText className='my-1'>
-                    <span className='mr-1'>
-                        <GithubButton
-                            href={ 'https://github.com/' + repo }
-                            data-show-count={ true } data-icon='octicon-star'
-                        >Star</GithubButton>
-                    </span>
+        <CardBody>
+            <GatsbyImage fluid={ fluid } style={ { maxWidth: 400, margin: 'auto' } }/>
+            <CardText className='my-1'>
+                <h3 style={ { display: 'inline', marginRight: 10 } }>{ name }</h3>
+                { repo ? <>
+                        <span className='mr-1'>
+                            <GithubButton
+                                href={ 'https://github.com/' + repo }
+                                data-show-count={ true } data-icon='octicon-star'
+                            >Star</GithubButton>
+                        </span>
                     <GithubButton
                         href={ `https://github.com/${ repo }/fork` }
                         data-show-count={ true } data-icon='octicon-repo-forked'
                     >Fork</GithubButton>
-                    <br/>
-                    <b>{ techonologies.join(', ') }</b>
-                    <br/>
-                    { description }
-                </CardText>
-            </CardBody>
-        </a>
+                </> : null }
+                <br/>
+                { description }
+                <br/>
+                <a href={ link } target='_blank' rel='noopener noreferrer' style={ { color: 'blue' } }>{ (new URL(link)).hostname }</a>
+            </CardText>
+        </CardBody>
     </Card>
 );
 
@@ -44,31 +45,39 @@ export default ({ data }) => {
             </Helmet>
             <main className='d-flex flex-wrap' id='main'>
                 <Row>
-                    <Col md={ 4 } sm={ 6 }>
+                    <Col md={ 6 } sm={ 12 }>
                         <Project
                             fluid={ getImage('students-review') }
-                            techonologies={ [ 'MERN (w/ gatsby)', 'python', 'graphql' ] }
+                            name='studentsreview.me'
                             repo='kajchang/studentsreview.me'
                             link='https://studentsreview.me'
-                            description='Building a website for Lowell High School students to see what which classes teachers teach and read and write reviews for teachers.'
+                            description='Website for Lowell High School students to see what which classes teachers teach and read and write reviews for teachers.'
                         />
                     </Col>
-                    <Col md={ 4 } sm={ 6 }>
+                    <Col md={ 6 } sm={ 12 }>
+                        <Project
+                            fluid={ getImage('mock-primary') }
+                            name='Lowell Mock Primary'
+                            link='https://vote.lowellhs.org'
+                            description="Interactive website to view primary results from Lowell's Mock Primary that I co-organized."
+                        />
+                    </Col>
+                    <Col md={ 6 } sm={ 12 }>
                         <Project
                             fluid={ getImage('studentvue') }
-                            techonologies={ [ 'python', 'web scraping' ] }
+                            name='StudentVue API'
                             repo='kajchang/StudentVue'
                             link='https://github.com/kajchang/StudentVue'
-                            description="Building an interface for students to programmatically query data from their district's student portal for their own applications."
+                            description="Interface for students to programmatically query data from their district's student portal for their own applications."
                         />
                     </Col>
-                    <Col md={ 4 } sm={ 6 }>
+                    <Col md={ 6 } sm={ 12 }>
                         <Project
                             fluid={ getImage('sfpl') }
-                            techonologies={ [ 'python', 'web scraping' ] }
+                            name='SFPL API'
                             repo='kajchang/SFPL'
                             link='https://github.com/kajchang/SFPL'
-                            description='Building San Franciscans to programmatically query library data and perform actions such as holding books.'
+                            description='Interface for San Franciscans to programmatically query library data and perform actions such as holding books.'
                         />
                     </Col>
                 </Row>
@@ -87,7 +96,7 @@ export const query = graphql`
             	node {
             	    name
               	    childImageSharp {
-                	    fluid(maxWidth: 550) {
+                	    fluid(maxWidth: 400) {
                   	        ...GatsbyImageSharpFluid
               		    }
   	        		}
