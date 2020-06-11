d5eac1b73d6b8048b5573fda1774c9b88062eece
Kai Chang <kaijchang@gmail.com>
2020-06-10 21:11:47 -0700

    change website

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,38 +9,80 @@ const MyOutboundLink = ({ children, href, ...props }) => <OutboundLink href={ hr
     { children }
 </OutboundLink>
 
+const Directory = ({ name, initiallyOpen = false, children }) => {
+    const [open, setOpen] = React.useState(initiallyOpen);
+
+    return (
+        <div className='flex flex-col'>
+            <div onClick={ () => setOpen(!open) } className='flex flex-row items-center cursor-pointer'>
+                <img
+                    src={ open ? '/opened-folder.svg' : '/folder.svg' }
+                    className='w-10 h-10 m-1 mr-4'
+                />
+                <span className='flex flex-col text-xl font-mono'>
+                    { name }
+                </span>
+            </div>
+            {
+                open ? (
+                    Array.isArray(children) ? children.map((child, idx) => (
+                        <div key={ idx } className='flex flex-row ml-10'>
+                            { child }
+                        </div>)
+                    ) : (
+                        <div className='flex flex-row ml-10'>
+                            { children }
+                        </div>
+                    )
+                ) : null
+            }
+        </div>
+    );
+}
+
+const ExternalLink = ({ name, icon, href }) => {
+    return (
+        <MyOutboundLink href={ href } className='flex flex-row items-center cursor-pointer'>
+            <img
+                src={ icon || '/external-link.svg' }
+                className='w-6 h-6 m-2'
+            />
+            <span className='flex flex-col text-xl font-mono leading-none'>
+                { name }
+            </span>
+        </MyOutboundLink>
+    );
+}
+
 export default () => {
     return (
         <>
             <Helmet>
-                <title>website</title>
+                <title>/</title>
             </Helmet>
-            <div className='flex flex-col items-center mt-20 mx-auto text-center'>
-                <h1 className='text-3xl font-bold'>Kai Chang • 张俊洙</h1>
-                <div className='w-7/8 md:w-1/2'>
-                    <div className='my-8'>
-                        <h2 className='text-2xl font-bold'>my links</h2>
-                        <ul className='flex flex-row flex-wrap justify-center list-horiz text-md'>
-                            <li><MyOutboundLink href='https://github.com/kajchang'>Github</MyOutboundLink></li>•
-                            <li><MyOutboundLink href='https://www.linkedin.com/in/kai-j-chang'>Linkedin</MyOutboundLink></li>•
-                            <li><MyOutboundLink href='https://blog.kaijchang.com'>Blog</MyOutboundLink></li>•
-                            <li><MyOutboundLink href='mailto:kaijchang@gmail.com'>Email</MyOutboundLink></li>•
-                            <li><MyOutboundLink href='https://www.strava.com/athletes/kachang'>Strava</MyOutboundLink></li>
-                        </ul>
-                    </div>
-                    <div className='my-8'>
-                        <h2 className='text-2xl font-bold'>links to things i've worked on</h2>
-                        <ul className='flex flex-row flex-wrap justify-center list-horiz text-md'>
-                            <li><MyOutboundLink href='https://github.com/Team4159'>Robotics</MyOutboundLink></li>•
-                            <li><MyOutboundLink href='https://studentsreview.me'>studentsreview.me (Lowell High School Teacher Reviews)</MyOutboundLink></li>•
-                            <li><MyOutboundLink href='https://vote.lowellhs.org'>2020 Lowell High School Mock Primaries</MyOutboundLink></li>•
-                            <li><MyOutboundLink href='https://arena.lowellhs.com'>Arena Rolodex</MyOutboundLink></li>•
-                            <li><MyOutboundLink href='https://github.com/StudentVue-Community'>StudentVue Developer Group</MyOutboundLink></li>•
-                            <li><MyOutboundLink href='https://github.com/kajchang/SFPL'>San Francisco Public Library API</MyOutboundLink></li>•
-                            <li><MyOutboundLink href='https://firebrand.kaijchang.com'>Firebrand (ELO Ratings for Politicians)</MyOutboundLink></li>
-                        </ul>
-                    </div>
-                </div>
+            <div className='ml-3'>
+                <Directory initiallyOpen={ true } name='/'>
+                    <Directory initiallyOpen={ true } name='links'>
+                        <ExternalLink name='github' icon='/github.png' href='https://github.com/kajchang'/>
+                        <ExternalLink name='devpost' icon='/devpost.svg' href='https://devpost.com/kajchang'/>
+                        <ExternalLink name='linkedin' icon='/linkedin.png' href='https://www.linkedin.com/in/kai-j-chang'/>
+                        <ExternalLink name='strava' icon='/strava.png' href='https://strava.com/athletes/kachang'/>
+                    </Directory>
+                    <Directory name='projects'>
+                        <ExternalLink name='Elo* Ratings for Politicians' href='https://firebrand.kaijchang.com'/>
+                        <ExternalLink name='StudentVue APIs' icon='/github.png' href='https://github.com/Studentvue-Community'/>
+                        <ExternalLink name='SFPL API' icon='/github.png' href='https://github.com/kajchang/SFPL'/>
+                        <ExternalLink name='react-device-mockups' icon='/github.png' href='https://github.com/kajchang/react-device-mockups'/>
+                        <Directory name='school related'>
+                            <ExternalLink name='2020 Mock Election' href='https://vote.lowellhs.org'/>
+                            <ExternalLink name='Teacher Reviews Site' href='https://studentsreview.me'/>
+                            <ExternalLink name='Arena Rolodex' href='https://arena.lowellhs.com'/>
+                        </Directory>
+                        <Directory name='hackathons'>
+                            <ExternalLink name='Green Space' href='https://mygreen.space'/>
+                        </Directory>
+                    </Directory>
+                </Directory>
             </div>
         </>
     )
