ec36876eaa5db89fadc02d1ab88584f338f99152
Kai Chang <kaijchang@gmail.com>
2021-07-17 23:51:41 -0700

    remove google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,11 +8,5 @@ module.exports = {
         },
         `gatsby-plugin-postcss`,
         `gatsby-plugin-typescript`,
-        {
-            resolve: `gatsby-plugin-google-analytics`,
-            options: {
-                trackingId: 'UA-140917974-3'
-            }
-        }
     ]
 };
