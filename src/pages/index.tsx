958345afa4e69397d72eafd81b16783c831b5049
Kai Chang <kaijchang@gmail.com>
2022-03-28 01:01:26 -0400

    prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -207,14 +207,16 @@ const PlaceSelector: React.FC<{
       {Object.values(visiblePlacesById).length === 0 && (
         <option value={DEFAULT_PLACE.id}>{DEFAULT_PLACE.text}</option>
       )}
-      {Object.values(visiblePlacesById).sort((a, b) => distanceByPlace[b.id] - distanceByPlace[a.id]).map((place, idx) => {
-        return (
-          <option key={idx} value={place.id}>
-            {place.text} (
-            {formatMilesDistance(metersToMiles(distanceByPlace[place.id]))})
-          </option>
-        )
-      })}
+      {Object.values(visiblePlacesById)
+        .sort((a, b) => distanceByPlace[b.id] - distanceByPlace[a.id])
+        .map((place, idx) => {
+          return (
+            <option key={idx} value={place.id}>
+              {place.text} (
+              {formatMilesDistance(metersToMiles(distanceByPlace[place.id]))})
+            </option>
+          )
+        })}
     </Select>
   )
 }
@@ -442,7 +444,24 @@ const RunOverlay: React.FC<{
   }, [activitiesByYear])
 
   return (
-    <Stack direction={{ base: 'row', md: 'column' }} position="fixed" zIndex={2} right={0} left={{ base: 0, md: 'auto' }} top={{ md: 0 }} bottom={0} overflowY="scroll" my={{ base: 4, md: 12 }} mr={4} ml={{ base: 4, md: 0 }} p={4} background="white" borderColor="black" borderWidth={2} rounded="md">
+    <Stack
+      direction={{ base: 'row', md: 'column' }}
+      position="fixed"
+      zIndex={2}
+      right={0}
+      left={{ base: 0, md: 'auto' }}
+      top={{ md: 0 }}
+      bottom={0}
+      overflowY="scroll"
+      my={{ base: 4, md: 12 }}
+      mr={4}
+      ml={{ base: 4, md: 0 }}
+      p={4}
+      background="white"
+      borderColor="black"
+      borderWidth={2}
+      rounded="md"
+    >
       {((Object.keys(statsByYear) as unknown) as number[])
         .sort((a, b) => +b - +a)
         .map((year, idx) => (
@@ -451,9 +470,7 @@ const RunOverlay: React.FC<{
             mr={{ base: 4, md: 0 }}
             key={idx}
           >
-            <chakra.button
-              opacity={visibleYears[year] ? 1 : 0.5}
-            >
+            <chakra.button opacity={visibleYears[year] ? 1 : 0.5}>
               <Heading
                 className="text-4xl leading-tight tracking-widest italic"
                 onClick={() => setIsYearVisible(year, !visibleYears[year])}
@@ -463,8 +480,12 @@ const RunOverlay: React.FC<{
             </chakra.button>
             <chakra.div>
               <chakra.p>{statsByYear[year][3]} runs</chakra.p>
-              <chakra.p>{(statsByYear[year][0] / 60 / 60).toFixed(2)} hrs</chakra.p>
-              <chakra.p>{formatMilesDistance(metersToMiles(statsByYear[year][1]))}</chakra.p>
+              <chakra.p>
+                {(statsByYear[year][0] / 60 / 60).toFixed(2)} hrs
+              </chakra.p>
+              <chakra.p>
+                {formatMilesDistance(metersToMiles(statsByYear[year][1]))}
+              </chakra.p>
               <chakra.p>
                 {Math.round(
                   metersToFeet(statsByYear[year][2])
