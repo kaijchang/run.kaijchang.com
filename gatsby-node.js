22f56fd2589de852fe1cfd37d1ac572ad0f490a5
Kai Chang <kaijchang@gmail.com>
2025-05-18 11:51:39 -0400

    error handling

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,9 +9,13 @@ exports.onCreateNode = async ({ node, actions, cache }) => {
     if (!data && node.activity) {
       let res;
       do {
-        res = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${node.activity.start_latlng[1]},${node.activity.start_latlng[0]}.json?access_token=${process.env.GATSBY_MAPBOX_TOKEN}`);
+        res = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${node.activity.start_latlng[1]},${node.activity.start_latlng[0]}.json?access_token=${process.env.GATSBY_MAPBOX_TOKEN}`)
         await new Promise(r => setTimeout(r, 60 * 1000 / 600 * 2))
       } while (res.status === 429)
+
+      if (res.status !== 200) {
+        throw new Error(`Error fetching geocoding data: ${res.status} ${res.statusText}\nResponse: ${JSON.stringify(res.json())}\nActivity: ${JSON.stringify(node.activity)}`)
+      }
 
       data = await res.json()
     }
--- src/utils/geojson.ts
+++ src/utils/geojson.ts
@@ -2,9 +2,9 @@ import polyline from '@mapbox/polyline'
 import { Run } from '../types'
 
 export const activityToFeature = (activity: Run) =>
-({
-  id: activity.id,
-  type: 'Feature',
-  geometry: polyline.toGeoJSON(activity.map!.summary_polyline),
-  properties: activity,
-} as GeoJSON.Feature<GeoJSON.LineString, Run>)
+  ({
+    id: activity.id,
+    type: 'Feature',
+    geometry: polyline.toGeoJSON(activity.map!.summary_polyline),
+    properties: activity,
+  } as GeoJSON.Feature<GeoJSON.LineString, Run>)
